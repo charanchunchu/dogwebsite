dded5fe2c38335e0f10ac8a3b4abc25f78b11329
charanchunchu <ch.charan98@gmail.com>
2022-04-25 15:32:09 +0530

    initial heroku commit

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 //install express srever
 const express =require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
+
 
 // serve only the static file form the dist directory
-app.use(express.ststic('./dist/project'));
+app.use(express.static(__dirname + './dist/project'));
 
-app.get('/*',(req,res) =>
- res.sendFile('index.html',{root:'dist/angular-heroku/'}),
- );
+//start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 
- //start the app by listening on the default Heroku port
- app.listen(process.env.PORT || 8080);
+app.get('/*', function(_req, res) {
+ res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
+console.log('console listening!');
